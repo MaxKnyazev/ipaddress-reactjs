2886a047417af4692cc73b40c220038b62641c81
MaxKnyazev <max.knyazev77@gmail.com>
2020-07-24 11:24:02 +0300

    2020-07-24 first commit after rename repo

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import classes from './Spinner.module.css'
+import classes from './Spinner.module.css';
 
 const Spinner = () => {
   return (
     <div>
-      <div className = {classes.loadingSpinner}>
-        <div className = {classes.ldio}>
+      <div className={classes.loadingSpinner}>
+        <div className={classes.ldio}>
           <div></div>
           <div></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Spinner;
-
-
-
-
-
-
-
-
