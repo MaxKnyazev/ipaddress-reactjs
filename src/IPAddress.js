3283b7bbdc71eb93e501d4676f29fb5aafffc72c
MaxKnyazev <max.knyazev77@gmail.com>
2020-09-08 11:37:44 +0300

    2020-09-08 set new font size

--- src/IPAddress.js
+++ src/IPAddress.js
@@ -4,20 +4,20 @@ import classes from './IPAddress.module.css';
 const IPAddress = ({ ip, city, country, loc, org, postal, timezone }) => {
   return (
     <>
-      <div>IP address :</div>
-      <div className={classes.colorInfo}>{ip}</div>
-      <div>Timezone :</div>
-      <div className={classes.colorInfo}>{timezone}</div>
-      <div>Country :</div>
-      <div className={classes.colorInfo}>{country}</div>
-      <div>Postal :</div>
-      <div className={classes.colorInfo}>{postal}</div>
-      <div>City :</div>
-      <div className={classes.colorInfo}>{city}</div>
-      <div>Location :</div>
-      <div className={classes.colorInfo}>{loc}</div>
-      <div>Organization :</div>
-      <div className={classes.colorInfo}>{org}</div>
+      <div className={classes.title}>IP address :</div>
+      <div className={classes.info}>{ip}</div>
+      <div className={classes.title}>Timezone :</div>
+      <div className={classes.info}>{timezone}</div>
+      <div className={classes.title}>Country :</div>
+      <div className={classes.info}>{country}</div>
+      <div className={classes.title}>Postal :</div>
+      <div className={classes.info}>{postal}</div>
+      <div className={classes.title}>City :</div>
+      <div className={classes.info}>{city}</div>
+      <div className={classes.title}>Location :</div>
+      <div className={classes.info}>{loc}</div>
+      <div className={classes.title}>Organization :</div>
+      <div className={classes.info}>{org}</div>
     </>
   );
 };
